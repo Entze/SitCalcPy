9598a5838b4d4d15876948f28423ac4819e37e04
Lukas Grassauer <lukas@grassauer.eu>
2022-08-02 12:54:45 +0200

    add do for public assignment

--- scpy/causal_setting/palat_causal_setting.py
+++ scpy/causal_setting/palat_causal_setting.py
@@ -1,12 +1,14 @@
-from typing import Union
+from typing import Union, Sequence
 
 from frozendict import frozendict  # type: ignore
 from pydantic.dataclasses import dataclass
 
 from scpy.action.action import Action
+from scpy.assignment.assignment import Assignment
 from scpy.causal_setting.epistemic_causal_setting import EpistemicCausalSetting
 from scpy.formula.evaluable_formula import EvaluableFormula
 from scpy.function.function import Function
+from scpy.literal.literal import Literal
 from scpy.situation.epistemic_situation import EpistemicSituation
 from scpy.situation.situation import Situation
 from scpy.state.state import State
@@ -24,12 +26,28 @@ class PalatCausalSetting(EpistemicCausalSetting):
             assert action.symbol == 'public_assignment'
             assert len(action.arguments)
             assert isinstance(action.arguments[0], Function)
-            # TODO
-            # For every fluent p and for every assignment every p := psi: If psi evaluates to True
-            # or
-            # If p was already true and not at least one of the assignments holds
-            pass
-        pass
+            _assignments: Function = action.arguments[0]
+            assert _assignments.symbol is None
+            assignments: Sequence[Assignment] = tuple(
+                argument for argument in _assignments.arguments if isinstance(argument, Assignment))
+            state_ = set(state)
+            for fluent in self.fluents:
+                literal = Literal(fluent)
+                for assignment in assignments:
+                    if assignment.predicate != fluent:
+                        continue
+                    assert isinstance(assignment.formula, EvaluableFormula)
+                    if assignment.formula.evaluate(state):
+                        if literal not in state:
+                            state_.add(literal)
+                        if -literal in state:
+                            state_.remove(-literal)
+                    else:
+                        if literal in state:
+                            state_.remove(literal)
+                        if -literal not in state:
+                            state_.add(-literal)
+            return frozenset(state_)
 
     def do(self, action: Action, situation: EpistemicSituation) -> EpistemicSituation:
         if action.symbol == 'public_announcement':
@@ -69,8 +87,20 @@ class PalatCausalSetting(EpistemicCausalSetting):
                                             knowledge_relation=knowledge_relation_)
             return situation_
         if action.symbol == 'public_assignment':
-            pass
-        pass
+            state_ = self.do_state(action, situation.state)
+            knowledge_relation = situation.knowledge_relation
+            knowledge_relation_ = frozendict(
+                {agent:
+                    frozendict({
+                        self.do_state(action, k_state): log_probability for k_state, log_probability in
+                        relation.items()}) for agent, relation in knowledge_relation.items()
+                })
+            situation_ = EpistemicSituation(state=state_,
+                                            previous_action=action,
+                                            previous_situation=situation,
+                                            knowledge_relation=knowledge_relation_)
+            return situation_
+        assert False
 
     def __poss_agnostic(self, action: Action, s: Union[State, Situation]) -> bool:
         if action.symbol == 'public_announcement':
